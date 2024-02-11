5e5d00211ccf3d446304dc71a6fde9b3a36c7f88
Marcin <marcinzybura6@wp.pl>
2024-02-11 15:25:12 +0100

    dodanie opcji usuwania zadań

--- to_do_list/main.js
+++ to_do_list/main.js
@@ -20,20 +20,37 @@ function displayItems() {
     let items = '';
     for (let i = 0; i < itemsArray.length; i++) {
         items += `<div class="item">
-                    <div class="input-controller">
-                        <textarea disabled>${itemsArray[i]}</textarea>
-                        <div class="edit-controller">
-                            <i class="fa-solid fa-check deleteBtn"></i>
-                            <i class="fa-regular fa-pen-to-square editBtn"></i>
+                        <div class="input-controller">
+                            <textarea disabled>${itemsArray[i]}</textarea>
+                            <div class="edit-controller">
+                                <i class="fa-solid fa-check deleteBtn"></i>
+                                <i class="fa-regular fa-pen-to-square editBtn"></i>
+                            </div>
                         </div>
-                    </div>
-                    <div class="update-controller">
-                        <button class="saveBtn">Save</button>
-                        <button class="cancelBtn">Cancel</button>
-                    </div>
-                    </div>`
+                        <div class="update-controller">
+                            <button class="saveBtn">Save</button>
+                            <button class="cancelBtn">Cancel</button>
+                        </div>
+                </div>`
     }
     document.querySelector(".to-do-list").innerHTML = items;
+    activateDelteListener();
+    activateEditListener();
+    activateSaveListener();
+    activateCancelListener();
+}
+
+function activateDelteListener() {
+    let deleteBtn = document.querySelectorAll(".deleteBtn")
+    deleteBtn.forEach((db, i) => {
+        db.addEventListener("click", () => { deleteItem(i) })
+    })
+}
+
+function deleteItem(i) {
+    itemsArray.splice(i, 1);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
 }
 
 function displayDate() {
