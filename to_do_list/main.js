be586cf575e0100a51153494edbb65a3795489da
Marcin <marcinzybura6@wp.pl>
2024-02-18 17:03:46 +0100

    funkcja edit, cancel oraz save

--- to_do_list/main.js
+++ to_do_list/main.js
@@ -54,8 +54,43 @@ function deleteItem(i) {
 }
 
 function activateEditListener() {
-    let editBtn = document.querySelectorAll(".editBtn")
-    editBtn.forEach()
+    let editBtn = document.querySelectorAll(".editBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll('.input-controller textarea');
+    editBtn.forEach((eb, i) => {
+        eb.addEventListener("click", () => {
+            updateController[i].style.display = "block";
+            inputs[i].disabled = false;
+        })
+    })
+}
+
+function activateSaveListener() {
+    const saveBtn = document.querySelectorAll(".saveBtn");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    saveBtn.forEach((sb, i) => {
+        sb.addEventListener("click", () => {
+            updateItem(inputs[i].value, i)
+        })
+    })
+}
+
+function activateCancelListener() {
+    const cancelBtn = document.querySelectorAll(".cancelBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    cancelBtn.forEach((cb, i) => {
+        cb.addEventListener("click", () => {
+            updateController[i].style.display = "none";
+            inputs[i].disabled = true;
+        })
+    })
+}
+
+function updateItem(text, i) {
+    itemsArray[i] = text;
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    location.reload();
 }
 
 function displayDate() {
